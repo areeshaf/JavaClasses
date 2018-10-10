9e9b8fd12afe342aba59eaeeab53e7a85535af33
areeshaFatima <areeshaf7@gmail.com>
2018-10-11 03:33:43 +0530

    Updated

--- Generics/src/Main.java
+++ Generics/src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 		Team<FootBallPlayer> madrid=new Team<>("Madrid");
 		madrid.addPlayers(ronaldo);
 		//madrid.addPlayers(abc);validation by adding generic class
-		Team<FootBallPlayer> brazil=new Team<>("brazil");
+		Team<FootBallPlayer> brazil=new Team<>("Brazil");
 		brazil.addPlayers(messi);
 		
 		
@@ -32,6 +32,20 @@ public class Main {
 		madrid.matchResult(brazil, 3, 2);//football
 		abcd.matchResult(wxyz, 1, 2);//soccer
 		efgh.matchResult(ijkl, 2, 2);//basketball
+		madrid.matchResult(brazil, 4, 2);
+		madrid.matchResult(brazil, 1, 3);
+		abcd.matchResult(wxyz, 2, 3);
+		
+		System.out.println("Rankings");
+		System.out.println("Football");
+		System.out.println(madrid.getName()+" : "+madrid.ranking());
+		System.out.println(brazil.getName()+" : "+brazil.ranking());
+		System.out.println("Soccer");
+		System.out.println(abcd.getName()+" : "+abcd.ranking());
+		System.out.println(wxyz.getName()+" : "+wxyz.ranking());
+		System.out.println("Basketball");
+		System.out.println(efgh.getName()+" : "+efgh.ranking());
+		System.out.println(ijkl.getName()+" : "+ijkl.ranking());
 		
 		
 		System.out.println("Number of Players in Football Team : "+madrid.getNumberofPlayers());
@@ -40,6 +54,11 @@ public class Main {
 		System.out.println("Number of Players in Soccer Team : "+wxyz.getNumberofPlayers());
 		System.out.println("Number of Players in Basketball Team : "+efgh.getNumberofPlayers());
 		System.out.println("Number of Players in Basketball Team : "+ijkl.getNumberofPlayers());
+		
+		System.out.println(brazil.compareTo(madrid));
+		System.out.println(abcd.compareTo(wxyz));
+		System.out.println(efgh.compareTo(ijkl));
+		
 	}
 
 }
