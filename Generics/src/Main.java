68a0dfc6e93c8715bf0b5b4d1d8038dd9d4c0a0d
areeshaFatima <areeshaf7@gmail.com>
2018-10-11 02:53:56 +0530

    Updated

--- Generics/src/Main.java
+++ Generics/src/Main.java
@@ -2,8 +2,25 @@
 public class Main {
 
 	public static void main(String[] args) {
+		FootBallPlayer ronaldo=new FootBallPlayer("Ronaldo");
+		SoccerBallPlayer abc=new SoccerBallPlayer("Abc");
+		BasketBallPlayer usher=new BasketBallPlayer("Usher");
 		
-
+		Team<FootBallPlayer> madrid=new Team<>("Madrid");
+		madrid.addPlayers(ronaldo);
+		//madrid.addPlayers(abc);validation by adding generic class
+		
+		Team<SoccerBallPlayer> abcd=new Team<>("ABCD");
+		abcd.addPlayers(abc);
+		Team<BasketBallPlayer> efgh=new Team<>("EFGH");
+		efgh.addPlayers(usher);
+		
+		
+		
+		
+		System.out.println("Number of Players in Football Team : "+madrid.getNumberofPlayers());
+		System.out.println("Number of Players in Soccer Team : "+abcd.getNumberofPlayers());
+		System.out.println("Number of Players in Basketball Team : "+efgh.getNumberofPlayers());
 	}
 
 }
